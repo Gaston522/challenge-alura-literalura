418459c1d64fe7c2e7886c0032f9af185475d17e
Gaston522 <gastoncoyant@gmail.com>
2024-05-09 22:12:55 -0300

    creacion del proyecto

---
+++ src/test/java/com/gc/LiterAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
